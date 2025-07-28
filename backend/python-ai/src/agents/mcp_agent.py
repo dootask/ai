d94738b7c0b69c74afa593c937ea54c2c69aac6d
zzzzzhy <11657924@qq.com>
2025-07-28 17:22:33 +0800

    feat: mcp agent添加prompt

--- backend/python-ai/src/agents/mcp_agent.py
+++ backend/python-ai/src/agents/mcp_agent.py
@@ -1,5 +1,5 @@
 from core import get_model_by_provider, settings
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage,SystemMessage
 from langchain_core.runnables import RunnableConfig
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.func import entrypoint
@@ -19,14 +19,14 @@ async def mcp_agent(
     # 1. 合并历史消息
     # if previous:
     #     messages = previous["messages"] + messages
-
+    configurable = config.get("configurable",{})
     # 2. 动态决定模型
     model = get_model_by_provider(
-        config.get("configurable",{}).get("provider", "openai"),
-        config.get("configurable",{}).get("model", settings.DEFAULT_MODEL),
-        config.get("configurable",{}).get("agent_config", None),
+        configurable.get("provider", "openai"),
+        configurable.get("model", settings.DEFAULT_MODEL),
+        configurable.get("agent_config", None),
     )
-    config_tuple = config.get("configurable",{}).get("mcp_config", None)
+    config_tuple = configurable.get("mcp_config", None)
     if config_tuple is None:
         config = {}
     mcp_config = dict(config_tuple) if config_tuple else {}
@@ -50,6 +50,7 @@ async def mcp_agent(
     )
     builder.add_edge("tools", "call_model")
     graph = builder.compile()
+    messages = [SystemMessage(content=configurable.get("agent_config").get("prompt",""))] + messages
     response = await graph.ainvoke({"messages": messages})
     final_messages = response["messages"]
     print(response)
